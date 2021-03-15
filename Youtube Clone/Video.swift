2fdd21951f23872191ea6e062e5b7e3019952305
Albanna14 <albanna1405@gmail.com>
2021-03-15 19:25:55 +0700

    Youtube Clone H1

---
+++ Youtube Clone/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  Youtube Clone
+//
+//  Created by Muhammad Azzam Al Banna on 15/03/21.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
